e256ee073e173aab5d1f3d55f213a81238fef0de
tedkkraft <tedkkraft@gmail.com>
2021-01-08 18:31:42 -0500

    react-player added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ReactPlayer from 'react-player'
+import { Container, Row, Col } from 'react-bootstrap';
 import LogoHeader from "./components/Logo-header";
 import WelcomeHeader from "./components/Welcome-header";
 import './App.css';
@@ -8,9 +10,15 @@ function App() {
     <div className="App">
       <WelcomeHeader />
       <LogoHeader />
-      <div className="Page-body">
+      <Container className="Page-body">
         <p>Test</p>
-      </div>
+        <ReactPlayer
+          url='https://static-email-hosting.s3.amazonaws.com/24G_Test_Project/videos/who_is_24g.mp4'
+          light="https://static-email-hosting.s3.amazonaws.com/24G_Test_Project/videos/who_is_24g.jpg"
+          playing
+          controls
+          />
+      </Container>
     </div>
   );
 }
