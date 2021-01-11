8251f84cfd0b58ee0b82fad68930cf1abaf31660
tedkkraft <tedkkraft@gmail.com>
2021-01-11 04:17:16 -0500

    Styling on footer

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import WelcomeHeader from "./components/Welcome-header";
 import LogoHeader from "./components/Logo-header";
 import ReactPlayer from 'react-player'
 // import Video from "./components/Video";
-import videos from "./videos.json";
 // import VideoThumb from "./components/Video-thumb";
+import videos from "./videos.json";
+import thumbsUp from './images/thumbs_up.svg'
+import thumbsDown from './images/thumbs_down.svg'
 import './App.css';
 
 class App extends React.Component {
@@ -32,6 +34,7 @@ class App extends React.Component {
               <p id="subheader" className="brandon-medium">24G Super Awesome Video</p>
             </Col>
           </Row>
+          {/* Video Player and Video Thumbnails */}
           <Row>
             <Col md={9}>
               <div className="video-wrapper">
@@ -58,24 +61,53 @@ class App extends React.Component {
               ))}
             </Col>
           </Row>
+          {/* Video Statistics */}
           <Row>
-            <Col md={9}>
-                <div className="video-stats">
-                  <p id="views-text"className="brandon-medium">300 views</p>
-                </div>
-                <div className="this-is-a-line-break">
-                </div>
+            <Col className="stats-row" md={9}>
+              {/* Views */}
+              <Row >
+                <Col>
+                  <div id="views">
+                    <p className="brandon-medium gray stats">300 views</p>
+                  </div>
+                </Col>
+                {/* Likes and Dislikes */}
+                <Col>
+                  <div className="float-right flex-row">
+                    <div id="like" className="flex-row stats">
+                      <img
+                        id="thumbs-up"
+                        className="thumb"
+                        src={thumbsUp}
+                        width="23"
+                        alt="thumbs up"
+                      />
+                      <p className="brandon-medium likes-text">35</p>
+                    </div>
+                    <div id="dislike" className="dislike flex-row stats">
+                      <img
+                        id="thumbs-down"
+                        className="thumb"
+                        src={thumbsDown}
+                        width="23"
+                        alt="thumbs down"
+                      />
+                      <p className="brandon-medium likes-text">4</p>
+                    </div>
+                  </div>
+                </Col>
+              </Row>
             </Col>
-            <Col md={3}>
-                <div className="spacing-column">
-                  <p>spacer col</p>
-                </div>
-            </Col>
+            {/* this column is for spacing purposes */}
+            <Col md={3} />
           </Row>
+          {/* Footer */}
           <Row>
-            <div className="text-center">
-                <p>powered by 24G</p>
-            </div>
+            <Col>
+              <div className="footer brandon-regular gray">
+                  <p>powered by <span className="brandon-bold">24G</span> </p>
+              </div>
+            </Col>
           </Row>
         </Container>
       </div>
--- src/components/Welcome-header.js
+++ src/components/Welcome-header.js
@@ -3,7 +3,7 @@ import { Navbar } from 'react-bootstrap';
 
 function WelcomeHeader() {
   return (
-    <Navbar id="welcome-header" className="brandon-light full-width-bar">
+    <Navbar id="welcome-header" className="brandon-light blue-bg full-width-bar">
       <p id="welcome-text">Welcome,<span className="brandon-bold"> Ted</span></p>
     </Navbar>
   )
